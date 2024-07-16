a7cd0e6fdc862bd2de43b2c0b2016be7c88e3300
diyar <diyar.soken@davincifascetti.it>
2024-07-16 21:13:44 +0200

    patch 0.2.4

--- src/me/diyar/ezarevents/utils/PermissionUtils.java
+++ src/me/diyar/ezarevents/utils/PermissionUtils.java
@@ -4,6 +4,6 @@ import me.diyar.ezarevents.Main;
 
 public class PermissionUtils {
 
-    public static String adminpermission = Main.getInstance().getConfig().getString("admin-permission");
-    public static String hostpermission = Main.getInstance().getConfig().getString("host-permission");
+    public static String adminpermission = Main.getInstance().getConfig().getString("ADMIN-PERMISSION");
+    public static String hostpermission = Main.getInstance().getConfig().getString("ADMIN-PERMISSION");
 }
