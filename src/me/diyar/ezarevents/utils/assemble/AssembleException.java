12384b2cfec029d3dcba0ca1b5fbe30c84ddc39c
Diyar <diyar.soken@davincifascetti.it>
2025-05-22 22:50:08 +0200

    update 22/05/2025

---
+++ src/me/diyar/ezarevents/utils/assemble/AssembleException.java
@@ -0,0 +1,14 @@
+package me.diyar.ezarevents.utils.assemble;
+
+public class AssembleException extends RuntimeException {
+
+    /**
+     * Assemble Exception.
+     *
+     * @param message attributed to exception.
+     */
+    public AssembleException(String message) {
+        super(message);
+    }
+
+}
